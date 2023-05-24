1053430469b478e12f853b8bf0aff976ce7dc1bc
ITDevSuccess <raharisontsidiany@gmail.com>
2023-05-24 23:55:23 +0300

    Lier les vues au urlsconfig

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -13,4 +13,8 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
+    return HttpResponse("You're voting at the results of question %s." % question_id)
+
+
+def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
