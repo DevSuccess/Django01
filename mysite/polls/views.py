eb85ae3b614c57ed3e4a9ca59143ecb8129c953c
ITDevSuccess <raharisontsidiany@gmail.com>
2023-05-25 09:49:38 +0300

    Mise en place de template pools/index.html

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hi, You're at the polls index.")
+    latest_question_list = Question.objects.order_dy("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list" : latest_question_list,
+    }
+
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
